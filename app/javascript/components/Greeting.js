887a74a2847b63d01a21162473286723d4b6ad26
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-07-04 20:22:29 +0530

    Added react pages to rails router

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -2,7 +2,7 @@ import React from 'react';
 const Greeting = () => {
   return (
     <React.Fragment>
-      <h1>Hello world for react</h1>
+      <h1>Hello world from react-rails</h1>
     </React.Fragment>
   );
 };
