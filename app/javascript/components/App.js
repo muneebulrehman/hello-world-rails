08fb00981e3d9c1072affb2b1e9ab5c450b18ee7
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-07-04 18:18:44 +0530

    Changed greeting to function based component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Greeting from './Greeting';
 
 const App = () => {
--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-class Greeting extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <h1>Hello world for react</h1>
-      </React.Fragment>
-    );
-  }
-}
+const Greeting = () => {
+  return (
+    <React.Fragment>
+      <h1>Hello world for react</h1>
+    </React.Fragment>
+  );
+};
 
 export default Greeting;
