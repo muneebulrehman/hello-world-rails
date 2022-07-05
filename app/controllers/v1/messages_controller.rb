efe9958ede88c38a79f877134677ea5f1a5ef47b
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-07-05 11:45:00 +0530

    Fixed linter errors

--- app/controllers/v1/messages_controller.rb
+++ app/controllers/v1/messages_controller.rb
@@ -1,6 +1,6 @@
 class V1::MessagesController < ApplicationController
   def index
     @messages = Message.all
-    render json: {message: @messages.sample.message}
+    render json: { message: @messages.sample.message }
   end
 end
